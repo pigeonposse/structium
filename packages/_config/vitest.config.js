3d565183924c5b5b222f0a2600e42bb992ba7f28
angelespejo <angelespejo13@gmail.com>
2025-07-19 19:23:14 +0200

    :green_heart: ci(env): fix playwright test in ci

--- packages/_config/vitest.config.js
+++ packages/_config/vitest.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig( { test : { projects : [
-	{ test : {
-		include     : [ 'src/**/*.{test,spec}.ts' ],
-		name        : 'unit',
-		environment : 'node',
-	} },
-	{ test : {
-		include : [ 'src/**/*.{test,spec}.ts' ],
-		name    : 'browser',
-		browser : {
-			provider           : 'playwright',
-			screenshotFailures : false,
-			connectTimeout     : 60000,
-			// enabled   : true,
-			headless           : true,
-			instances          : [ { browser: 'chromium' } ],
-		},
-	} },
-] } } )
+export default defineConfig( { test : {
+	testTimeout : 20000, // 20s
+	projects    : [
+		{ test : {
+			include     : [ 'src/**/*.{test,spec}.ts' ],
+			name        : 'unit',
+			environment : 'node',
+		} },
+		{ test : {
+			include : [ 'src/**/*.{test,spec}.ts' ],
+			name    : 'browser',
+			browser : {
+				provider           : 'playwright',
+				screenshotFailures : false,
+				connectTimeout     : 60000, // 60s
+				// enabled   : true,
+				headless           : true,
+				instances          : [ { browser: 'chromium' } ],
+			},
+		} },
+	],
+} } )
